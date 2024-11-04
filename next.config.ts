d8101f4dbb803c152d96f47f2f91fbbf6e151274
Marian Hello <marian.hello@colonnade.sk>
2024-11-04 20:42:08 +0100

    feat: add webpack list assets names plugin

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpack: (config, { webpack }) => {
+    config.plugins.push({
+      apply: (compiler) => {
+        compiler.hooks.compilation.tap("LogFinalFilenamesPlugin", (compilation) => {
+          compilation.hooks.processAssets.tap(
+            {
+              name: "LogFinalFilenamesPlugin",
+              stage: webpack.Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+            },
+            () => {
+              const assetNames = Object.keys(compilation.assets);
+              console.log("Final asset filenames:", assetNames);
+            }
+          );
+        });
+      },
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
